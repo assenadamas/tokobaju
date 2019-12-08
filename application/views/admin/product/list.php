e46496719206d949c62e6019eb3fca822e6e7940
Damas Fahmi Assena <assenadamas7@gmail.com>
2019-12-08 17:07:03 +0700

    Mencari dataTable

--- application/views/admin/product/list.php
+++ application/views/admin/product/list.php
@@ -26,7 +26,7 @@
 					<div class="card-body">
 
 						<div class="table-responsive">
-							<table class="table table-hover" id="dataTable" width="100%" cellspacing="0">
+							<table class="table table-hover" id="dataassena" width="100%" cellspacing="0">
 								<thead>
 									<tr>
 										<th>Name</th>
